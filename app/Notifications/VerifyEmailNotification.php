186d302d76318c593450251e6d3df8496fdfc37e
Md. Arif Hassan <aarifhsn@gmail.com>
2026-07-05 10:51:55 +0600

    fixed verification url issues

--- app/Notifications/VerifyEmailNotification.php
+++ app/Notifications/VerifyEmailNotification.php
@@ -11,22 +11,13 @@ class VerifyEmailNotification extends VerifyEmail
 {
     protected function verificationUrl($notifiable): string
     {
-        $frontendUrl = config('app.frontend_url');
-
-        $backendUrl = URL::temporarySignedRoute(
+        return URL::temporarySignedRoute(
             'verification.verify',
             Carbon::now()->addMinutes(60),
             [
                 'id' => $notifiable->getKey(),
                 'hash' => sha1($notifiable->getEmailForVerification()),
             ]
-        );
-
-        // replace backend base with frontend so link goes to your app
-        return str_replace(
-            config('app.url') . '/api',
-            $frontendUrl,
-            $backendUrl
         );
     }
 }
